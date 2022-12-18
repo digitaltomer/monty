9aa198a91c86df1aff6ab84e2b98bcb4558ec395
Digitaltomer <tomamadin@gmail.com>
2022-12-18 21:27:23 +0100

    main fix

--- main.c
+++ main.c
@@ -1,29 +1,55 @@
 #include "monty.h"
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 
-char **op_toks = NULL;
+char **line_token = NULL;
 
 /**
- * main - the entry point for Monty Interp
- *
- * @argc: the count of arguments passed to the program
- * @argv: pointer to an array of char pointers to arguments
- *
- * Return: (EXIT_SUCCESS) on success (EXIT_FAILURE) on error
- */
-int main(int argc, char **argv)
-{
-	FILE *script_fd = NULL;
-	int exit_code = EXIT_SUCCESS;
+ * main - The starting point
+ * @ac: keep track of argument count
+ * @av: the argument vectors
+ * Return: EXIT_SUCCESS (0)
+*/
 
-	if (argc != 2)
-		return (usage_error());
-	script_fd = fopen(argv[1], "r");
-	if (script_fd == NULL)
-		return (f_open_error(argv[1]));
-	exit_code = run_monty(script_fd);
-	fclose(script_fd);
-	return (exit_code);
+int main(int ac, char *av[])
+{
+	char *line = NULL;
+	FILE *file;
+	size_t line_size = 0;
+	unsigned int line_number = 0;
+	void (*opcode_function)(stack_t **, unsigned int);
+	stack_t *stk = NULL;
+
+	argcount(ac);
+
+	file = fopen(av[1], "r");
+	checkfile(file, av[1]);
+
+	line = malloc(sizeof(char *));
+
+	while (getline(&line, &line_size, file) != -1)
+	{
+		line_number++;
+		line_token = tokenize(line);
+		if (line_token == NULL)
+		{
+			if (check_empty_line(line, DELIMITERS) == 1)
+				continue;
+			else
+				mallocerror();
+		}
+		if (line_token[0][0] == '#')
+			continue;
+		if (stk== NULL)
+			monty_stack_mode(&stk, line_number);
+		opcode_function = get_opcode(line_token[0]);
+		if (opcode_function != NULL)
+			opcode_function(&stk, line_number);
+		else
+			instructionerror(line_number);
+
+	}
+	free_stack(&stk);
+	free(line_token);
+	free(line);
+	fclose(file);
+	return (EXIT_SUCCESS);
 }
